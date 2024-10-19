c6a5386015196ad4fa04b39f3feb0374e654dc58
iokl1004 <iokl3319@naver.com>
2024-10-19 10:37:07 +0900

    20241019
    1. Favorite
    - 문구 수정 완료
    - Movie Title 클릭 시, 해당 영화정보 페이지로 이동되도록 수정
    
    2. Footer
    - Happy Coding 부분 삭제 (추후 디자인 예정)

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -3,6 +3,7 @@ import './favorite.css'
 import Axios from 'axios'
 import { Popover } from 'antd';
 import { IMAGE_BASE_URL } from '../../Config';
+import No_Image from '../commons/img/No_Image.png';
 
 function FavoritePage() {
 
@@ -40,11 +41,15 @@ function FavoritePage() {
         })
     }
 
+    const onClickMovie = () => {
+        alert("ㄹㅇㅋ");
+    }
+
     const renderCards = Favorites.map((favorite, index) => {
         const content = (
             <div>
                 {favorite.moviePost ?
-                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "noimage"
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : No_Image  // API 서버에 파일이 존재하지 않을 경우
                 }
             </div>
         )
@@ -52,9 +57,9 @@ function FavoritePage() {
         return (
             <tr key={index}>
                 <Popover content={content} title={`${favorite.movieTitle}`}>
-                    <td>{favorite.movieTitle}</td>
+                    <td><a href={`/movie/${favorite.movieId}`}>{favorite.movieTitle}</a></td>
                 </Popover>
-                <td>{favorite.movieRunTime} mins</td>
+                <td>{favorite.movieRunTime}분</td>
                 <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
             </tr>
         )
@@ -62,15 +67,15 @@ function FavoritePage() {
 
     return (
         <div style = {{ width: '85%', margin: '3rem auto' }}>
-            <h2> Favorite Movies </h2>
+            <h2> 나의 Favorite 영화 </h2>
             <hr />
 
             <table>
                 <thead>
                     <tr>
-                        <th>Movie Title</th>
-                        <th>Movie Runtime</th>
-                        <th>Remove from favorites</th>
+                        <th>제목</th>
+                        <th>러닝타임</th>
+                        <th>Favorites 제거</th>
                     </tr>
                 </thead>
                 <tbody>
