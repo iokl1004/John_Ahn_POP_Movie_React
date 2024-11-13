1f21a0d4e311ac849be7890cae3bee007a8b96a0
iokl1004 <iokl3319@naver.com>
2024-11-13 18:58:56 +0900

    20241113
    1. LandingPage
    
    - 마우스 hover시 영화 제목, 영화 평점, 영화 간략한 소개글 나오게끔 개발 (20241113 완료)
      - 마우스 hover 시 영화 제목 alert창 나오게끔 개발 (부모컴포넌트에서 하위컴포넌트로 props 전달 하는 구문 추가, 20241109 완료)
      - 부모컴포넌트에서 전달받은 props 값을 마우스 hover시 표시 (20241113 완료)
        - 마우스 hover 시, props값(영화 제목, 영화 평점, 영화 소개글) 표시
        - CSS 설정

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { Col } from 'antd';
 import No_Image from './img/No_Image.png';
+import styled from "styled-components";
 
 function GridCards(props) {
 
-    const handleMouseOver = () => {
-        alert(props.movieName);
-    }
+    const ImgWrapper = styled.div`
+        position : relative;
+
+        .movie_info {
+            position : absolute;
+            left : 5%;
+            right : 5%;
+            bottom : 0px;
+            z-index : 1;
+            font-size : 15px;
+            color : #ffffff;
+            font-weight : 700;
+            display : none;
+            // user-select : none;  // 텍스트 복사 방식
+        }
+
+        // Mouse hover시 이미지가 짙어지며 영화 관련 소개글이 보일 수 있도록 설정
+        &:hover .movie_info {
+            display : block;
+        }
+        
+        // Mouse hover시 해당 이미지가 짙어 질 수 있도록 설정
+        &:hover .post_thumbnail {
+            filter : brightness(0.5);
+        }
+    `;
 
     // 렌딩 페이지인 경우
     if(props.landingPage) {
@@ -15,11 +39,24 @@ function GridCards(props) {
             // 미디움 사이즈 인 경우에는 3개
             // 가장 작은 사이즈 인 경우에는 1개
             <Col lg={6} md={8} xs={24}>
-                <div style={{ position : 'relative' }}>
-                    <a href={`/movie/${props.movieId}`} onMouseOver={handleMouseOver}>
-                        <img style={{ width : '100%', height: '400px' }} src={props.image} alt={props.movieName} />
+                <ImgWrapper>
+                    <a href={`/movie/${props.movieId}`}>
+                        <p className='movie_info'>
+                            <div>
+                                영화 제목 : {props.movieName}
+                            </div>
+                            <div>
+                                평점 : {props.vote_average}
+                            </div>
+                            <div>
+                                소개글 : {props.overview}
+                            </div>
+                        </p>
+                        <div className='post_thumbnail'>
+                            <img style={{ width : '100%', height: '500px' }} src={props.image} alt={props.movieName} />
+                        </div>
                     </a>
-                </div>
+                </ImgWrapper>
             </Col>
         )
     } else {
